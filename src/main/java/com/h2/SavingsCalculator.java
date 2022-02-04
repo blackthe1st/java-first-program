1427520348afa0a7924aaa7596a751e7438917df
blackthe1st <blackthe1st@hotmail.com>
2022-02-04 09:10:24 +0300

    SavingsCalculator.java added credits and debits members added in SavingsCalculator.java sumOfCredits and sumOfDebits methods added and implemented

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,39 @@
+package com.h2;
+
+public class SavingsCalculator {
+
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits) {
+
+        this.credits = credits;
+        this.debits= debits;
+
+    }
+
+    private float sumOfCredits() {
+
+        float sum = 0.0f;
+
+        for(int i = 0; i < credits.length; i++) {
+            sum += credits[i];
+        }
+
+        return sum;
+
+    }
+
+    private float sumOfDebits() {
+
+        float sum = 0.0f;
+
+        for(int i = 0; i < debits.length; i++) {
+            sum += debits[i];
+        }
+
+        return sum;
+
+    }
+
+}
